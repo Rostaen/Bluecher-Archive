f391d6a612258b0312139ffbb482da3d33a88f8c
Andrew Fillhart <afillhar@bard.edu>
2024-02-28 15:55:05 -0500

    Updated transcripts pages for more OOP requirements.

--- trans_inv.php
+++ trans_inv.php
@@ -1,13 +1,17 @@
 <?php
-    require_once("template.php");
+require_once("template.php");
+require_once("transcriptData.php");
+$page = new Template();
+$page->setTitle('Lecture Transcripts | Blücher Archive | Bard College');
+$page->setMetaDesc('');
 
-	$page = new Template();
-	$page->setTitle('Lecture Transcripts | Blücher Archive | Bard College');
-	$page->setMetaKW('');
-	$page->setMetaDesc('');
+// print page header
+$page->open();
 
-	// print page header
-	$page->open();
+function printLI($item)
+{
+	echo '<li>' . $item . '</li>';
+}
 ?>
 </header>
 <div class="container home mt-4">
@@ -23,52 +27,10 @@
 		<h4>Blücher transcripts</h4>
 
 		<ol>
-			<li>
-				"Why and How Do We Study Philosophy?"<br>
-				1952 (Summer)<br>
-				209 Leaves (typescript)<br>
-				red loose leaf binding
-			</li>
-			<li>
-				"The Quest for God" Lectures I-VIII<br>
-				Volume 1 185 leaves (typescript)<br>
-				Fall 1952 red loose leaf binding
-			</li>
-			<li>
-				"The Quest For God" Lectures IX-XV<br>
-				Volume 2 leaves 187-359<br>
-				Fall 1952 red loose leaf binding
-			</li>
-			<li>
-				"Sources of Creative Power" 1954<br>
-				Appears to be duplicates of SCP lectures 2 vols. unpaged, approx. 200 leaves per black loose leaf binding [a note on one vol. indicates that it is vol. IV but only two vols. exist in this collection.]
-			</li>
-			<li>
-				Fundamentals of a philosophy of art on the understanding of artistic experience<br>
-				Spring 1951? inscribed on 1st page:<br>
-				Transcribed from reel-to-reel tapes<br>
-				137 loose leaves (photocopies) then 20 additional leaves typed (photocopied) in a single column dated 5-25-51 "ladies and gentlemen, we come to the end of this course that was intended..."
-			</li>
-			<li>
-				Fundamentals of the History of Art..."<br>
-				lectures 7-13 dated 1957<br>
-				loose leaf typescript collated into lectures
-			</li>
-			<li>
-				"Metaphysical Areas of Human Values in Modern Art II" (taken from container)<br>
-				lectures numbered 8-13 w/ handwritten notes<br>
-				156 loose pages collated into lectures
-			</li>
-			<li>
-				Metaphysical Foundations of Politics<br>
-				"Drafts"<br>
-				Lectures I-VIII<br>
-				Original transcripts on badly damaged paper approx. 250 leaves
-			</li>
-			<li>
-				"The Common Course"<br>
-				original mss. edited in HB's hand gift of Theodore Weiss 8-20-01
-			</li>
+			<?php
+			foreach ($transcriptData as $item)
+				printLI($item[0]);
+			?>
 		</ol>
 
 		<h2>List of Transcripts of Heinrich Blücher's Lectures, 1952-1959</h2>
@@ -76,52 +38,34 @@
 		<p>From a letter dated April 29, 1976, from Frederick G. Cook, Head Librarian, to Peter Skiff</p>
 
 		<ol>
-			<li>The Quest for God (Fall 1951 / Spring 1952)</li>
-			<li>Man Alone: Existential Thinking from Kierkegaard and Nietzche to Heidegger and Sartre (early fifties)</li>
-			<li>Metaphysical Foundations of Politics (Fall 1952)</li>
-			<li>Fundamentals of a Philosophy of Art. On the Understanding of Artistic Experience (early fifties)</li>
-			<li>The Myth of the Void: Landscape and Population. A Deomonology of the Modern Character (1952?)</li>
-			<li>Why and How do we study Philosophy? (Summer 1952)</li>
-			<li>Sources of Creative Power: Origins of Human Principles (Fall 1954 / Spring 1954)<br>
-    			NB: Here follows a note: "Here appear the nine figures which later became the subject of Bard's Common Course for the first time in the following order: Laotse, Buddha, Zarathustra, Abraham, Homer, Heraclitus, Solon, Socrates and Jesus."</li>
-			<li>The Human Trinity: Truth Faith Freedom (Fall 1954 / Spring 1955)</li>
-			<li>Metaphysical Ideas and Human Values in Modern Art (Fall 1956 / Spring 1957)</li>
+			<?php
+			foreach ($letterData as $item)
+				printLI($item[0]);
+			?>
 		</ol>
 
 		<h3>Lectures not Transcribed (as of 4/29/76)</h3>
 
 		<ol>
-			<li>The Quest for God (Fall 1955 / Spring 1956)</li>
-			<li>Modern Revolution of Human Experience (Fall 1957 / Spring 1958)</li>
-			<li>Ethical Confusion and Moral Corruption (Fall 1958 / Spring 1959)</li>
+			<?php
+			foreach ($lectureData as $item)
+				printLI($item[0]);
+			?>
 		</ol>
 
 		<h3>Individual Transcriptions</h3>
 
 		<p>Lectures arranged by Alexander Bazelow. These lectures were separated out from the other transcripts, bound, and given numbers in the order below. They seem to be an attempt to reconstruct, from lectures given both at the New School and at Bard, what the common course may have looked like. Some of them, such as the lectures on "Homer" from 1954, are drawn from the lecture series entitled "Sources of Creative Power." Though Blücher never delivered the course in the manner indicated here, and the arrangement of the lectures here may more accurately be an attempt to form a coherent thread out of the vast material at hand, this seems to give a reasonable idea of the look and feel of the Common Course as it was intended to be. The dates below are the dates the lectures were actually given.</p>
-		
+
 		<ol type="I">
-			<li>Introduction to the Common Course (1952)</li>
-			<li>Talk on the Common Course (1952)</li>
-			<li>Homer (3 Lectures) (1954)</li>
-			<li>Homer (1967)</li>
-			<li>Socrates (2 Lectures) (1954)</li>
-			<li>Heraclitus (1967)</li>
-			<li>Buddha (1967)</li>
-			<li>Jesus (2 Lectures) (1954)</li>
-			<li>Zarathustra (1954)</li>
-			<li>India and the Mythopoetic Mind of Man (1967)</li>
-			<li>Academic Freedom (1967)</li>
-			<li>Politics, Man, and Freedom (1967)</li>
-			<li>A Dialogue with Students (1968)</li>
-			<li>A Fragment on Kierkegaard (1952)</li>
-			<li>Last Lecture</li>
+			<?php
+			foreach ($individualData as $item)
+				printLI($item[0]);
+			?>
 		</ol>
 	</div>
 </div>
 
-
-
 <?php
-	$page->close();
+$page->close();
 ?>
---
+++ transcriptData.php
@@ -0,0 +1,49 @@
+<?php
+
+$transcriptData = [
+    ['"Why and How Do We Study Philosophy?"<br>1952 (Summer)<br>209 Leaves (typescript)<br>red loose leaf binding'],
+    ['"The Quest for God" Lectures I-VIII<br>Volume 1 185 leaves (typescript)<br>Fall 1952 red loose leaf binding'],
+    ['"The Quest For God" Lectures IX-XV<br>Volume 2 leaves 187-359<br>Fall 1952 red loose leaf binding'],
+    ['"Sources of Creative Power" 1954<br>Appears to be duplicates of SCP lectures 2 vols. unpaged, approx. 200 leaves per black loose leaf binding [a note on one vol. indicates that it is vol. IV but only two vols. exist in this collection.]'],
+    ['Fundamentals of a philosophy of art on the understanding of artistic experience<br>Spring 1951? inscribed on 1st page:<br>Transcribed from reel-to-reel tapes<br>137 loose leaves (photocopies) then 20 additional leaves typed (photocopied) in a single column dated 5-25-51 "ladies and gentlemen, we come to the end of this course that was intended..."'],
+    ['Fundamentals of the History of Art..."<br>lectures 7-13 dated 1957<br>loose leaf typescript collated into lectures'],
+    ['"Metaphysical Areas of Human Values in Modern Art II" (taken from container)<br>lectures numbered 8-13 w/ handwritten notes<br>156 loose pages collated into lectures'],
+    ['Metaphysical Foundations of Politics<br>"Drafts"<br>Lectures I-VIII<br>Original transcripts on badly damaged paper approx. 250 leaves'],
+    ['"The Common Course"<br>original mss. edited in HB\'s hand gift of Theodore Weiss 8-20-01'],
+];
+
+$letterData = [
+    ['The Quest for God (Fall 1951 / Spring 1952)'],
+    ['Man Alone: Existential Thinking from Kierkegaard and Nietzche to Heidegger and Sartre (early fifties)'],
+    ['Metaphysical Foundations of Politics (Fall 1952)'],
+    ['Fundamentals of a Philosophy of Art. On the Understanding of Artistic Experience (early fifties)'],
+    ['The Myth of the Void: Landscape and Population. A Deomonology of the Modern Character (1952?)'],
+    ['Why and How do we study Philosophy? (Summer 1952)'],
+    ['Sources of Creative Power: Origins of Human Principles (Fall 1954 / Spring 1954)<br>NB: Here follows a note: "Here appear the nine figures which later became the subject of Bard\'s Common Course for the first time in the following order: Laotse, Buddha, Zarathustra, Abraham, Homer, Heraclitus, Solon, Socrates and Jesus."'],
+    ['The Human Trinity: Truth Faith Freedom (Fall 1954 / Spring 1955)'],
+    ['Metaphysical Ideas and Human Values in Modern Art (Fall 1956 / Spring 1957)'],
+];
+
+$lectureData = [
+    ['The Quest for God (Fall 1955 / Spring 1956)'],
+    ['Modern Revolution of Human Experience (Fall 1957 / Spring 1958)'],
+    ['Ethical Confusion and Moral Corruption (Fall 1958 / Spring 1959)'],
+];
+
+$individualData = [
+    ['Introduction to the Common Course (1952)'],
+    ['Talk on the Common Course (1952)'],
+    ['Homer (3 Lectures) (1954)'],
+    ['Homer (1967)'],
+    ['Socrates (2 Lectures) (1954)'],
+    ['Heraclitus (1967)'],
+    ['Buddha (1967)'],
+    ['Jesus (2 Lectures) (1954)'],
+    ['Zarathustra (1954)'],
+    ['India and the Mythopoetic Mind of Man (1967)'],
+    ['Academic Freedom (1967)'],
+    ['Politics, Man, and Freedom (1967)'],
+    ['A Dialogue with Students (1968)'],
+    ['A Fragment on Kierkegaard (1952)'],
+    ['Last Lecture'],
+];
